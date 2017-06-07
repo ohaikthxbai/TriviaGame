caf688a2f51b029fbcc99f634792a542d7735386
Justin G <MBA13-JG@Justins-MacBook-Air.local>
2017-06-06 23:08:58 -0500

    finally got the question and choices to display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,9 +30,6 @@ have the user *start the game* by *pressing a button*
 
 */
 
-
-
-
 // when the web page is ready
 //$(document).ready(function(){
 
@@ -52,6 +49,8 @@ var trivialTrivia = {
 	// timer for each question
 	qTimer: 0,
 	tTimer: '',
+	//current question
+	qCurrent: -1,
 	// array of questions
 	qArray: [
 	// each element in the array will be an object
@@ -59,83 +58,55 @@ var trivialTrivia = {
 	{	// 1
 		question: "Test Question",
 		choices: ["zero", "one", "two", "three"],
-		answer: "one"
+		answer: 1
 	},
 
 	{	// 2
-		question: "Test Question",
+		question: "Another Test Question",
 		choices: ["zero", "one", "two", "three"],
-		answer: "two"
+		answer: 2
 	},
 
 	{	// 3
-		question: "Test Question",
+		question: "This Test Question",
 		choices: ["zero", "one", "two", "three"],
-		answer: "zero"
+		answer: 0
 	},	
 
 	{	// 4
-		question: "Test Question",
+		question: "That Test Question",
 		choices: ["zero", "one", "two", "three"],
-		answer: "three"
+		answer: 3
 	},
 
 	{	// 5
-		question: "Test Question",
+		question: "Final Test Question",
 		choices: ["zero", "one", "two", "three"],
-		answer: "one"
-	},
-/*
-	{	// 6
-		question: "Test Question",
-		choices: ["zero", "one", "two", "three"],
-		answer: "one"
+		answer: 1
 	},
 
-	{	// 7
-		question: "Test Question",
-		choices: ["zero", "one", "two", "three"],
-		answer: "three"
-	},
-
-	{	// 8
-		question: "Test Question",
-		choices: ["zero", "one", "two", "three"],
-		answer: "zero"
-	},	
-
-	{	// 9
-		question: "Test Question",
-		choices: ["zero", "one", "two", "three"],
-		answer: "two"
-	},
-
-	{	// 10
-		question: "Test Question",
-		choices: ["zero", "one", "two", "three"],
-		answer: "zero"
-	}
-*/
 	]
 }
+// random looking like it's not working
+//var qRandom = trivialTrivia.qArray[Math.floor((Math.random() * trivialTrivia.qArray.length))];
 
 // FUNCTIONS
 
 // starting the game
 var pressStart = function() {
 	$(".pStart").on("click", function(){
-		$(".gameArea").empty();
+		$(".gArea").empty();
 		qDisplay();
 	});
 }
 
-// timer functions
+// TIMER FUNCTIONS
 // start the timer
 var tStart = function() {
 	// clear the timer area
 	$(".timerArea").empty();
 	// set the timer value
-	trivialTrivia.qTimer = 20;
+	trivialTrivia.qTimer = 90;
 	// define the timer using setInterval
 	trivialTrivia.tTimer = setInterval(tDecrease, 1000);
 	//testing
@@ -152,7 +123,6 @@ var tDecrease = function() {
 	// stop the timer if it reachers zero
 	if(trivialTrivia.qTimer === 0) {
 		tStop();
-
 	}
 
 }
@@ -160,7 +130,7 @@ var tDecrease = function() {
 // displaying the timer
 var tDisplay = function() {
 	// display timer on the DOM
-	$(".timerArea").html(": "trivialTrivia.qTimer + " seconds remaining");
+	$(".timerArea").html(":" + trivialTrivia.qTimer);
 	//testing
 	//console.log(trivialTrivia.qTimer);
 }
@@ -171,12 +141,36 @@ var tStop = function() {
 	clearInterval(trivialTrivia.tTimer);
 }
 
-// display the current question
+// display the question and choices
 var qDisplay = function() {
 	// start the timer for the question
 	tStart();
-
-
+	// define random question
+	var qRandom = trivialTrivia.qArray[Math.floor((Math.random() * trivialTrivia.qArray.length))];
+	// make div element
+	var addDiv = $('<div>');
+	// giving addDiv a class
+	addDiv.addClass('gQuestion');
+	// adding the text
+	addDiv.text(qRandom.question);
+	// add question to html
+	$('.gArea').append(addDiv);
+	// display the choices
+	// interate through each choice and make a button for them
+	for (i = 0; i < qRandom.choices.length; i++){
+		// declare and add element to html
+        var addBtn = $("<div>")
+        // add classes to the element
+        addBtn.addClass("btn options");
+        //addBtn.data("num", i); 
+        //addBtn.on("click", compare);
+        // add text to the element
+        addBtn.text(qRandom.choices[i]);
+        // add it to the dom
+        $(".gArea").append(addBtn);
+    }
+	//testing
+	//console.log(qRandom.question)
 }
 
 // checks the player's selection
@@ -185,9 +179,12 @@ var qCheck = function() {
 }
 
 //testing
-//for (i = 0; i < trivialTrivia.qArray.length; i++)
-//console.log(trivialTrivia.qArray[i]);
 var qRandom = trivialTrivia.qArray[Math.floor((Math.random() * trivialTrivia.qArray.length))];
-console.log(qRandom);
+for (i = 0; i < qRandom.choices.length; i++) {
+console.log(qRandom.choices[i]);
+}
+//console.log(qRandom.question);
+//qDisplay();
+pressStart();
 
 //})
